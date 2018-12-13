5e26badd65e1d59bcf0617753abd3b28ff5683c7
Lukeveille <luke@lukeleveille.com>
2018-12-12 20:05:19 -0500

    fixed some toggle glitches

--- App.js
+++ App.js
@@ -5,9 +5,9 @@ function DefaultScreen(props) {
   return (
     <div>
       Highest integer you'd like to see:
-      <input ref={input => {input && props.focus === 'range'? input.focus() : ''}} id="range" value={props.range} type="text" size="5" autoComplete="off" onChange={e => props.updateInput(e)} onKeyUp={e => {e.keyCode == 13? props.initialize() : e.keyCode == 38 || e.keyCode == 40? props.toggleFocus() : e.keyCode == 39? props.toggleMathType(true) : e.keyCode == 37? props.toggleMathType(false) : ''}} />
+      <input ref={input => {input && props.focus === 'range'? input.focus() : ''}} id="range" value={props.range} type="text" size="5" autoComplete="off" onChange={e => props.updateInput(e)} onKeyUp={e => {e.keyCode == 13? props.initialize() : e.keyCode == 40? props.toggleFocus() : e.keyCode == 39? props.toggleMathType(true) : e.keyCode == 37? props.toggleMathType(false) : ''}} />
       How many questions?
-      <input ref={input => {input && props.focus === 'questions'? input.focus() : ''}} id="questions" value={props.questions} type="text" size="5" autoComplete="off" onChange={e => props.updateInput(e)} onKeyUp={e => {e.keyCode == 13? props.initialize() : e.keyCode == 38 || e.keyCode == 40? props.toggleFocus() : e.keyCode == 39? props.toggleMathType(true) : e.keyCode == 37? props.toggleMathType(false) : ''}} />
+      <input ref={input => {input && props.focus === 'questions'? input.focus() : ''}} id="questions" value={props.questions} type="text" size="5" autoComplete="off" onChange={e => props.updateInput(e)} onKeyUp={e => {e.keyCode == 13? props.initialize() : e.keyCode == 38? props.toggleFocus() : e.keyCode == 39? props.toggleMathType(true) : e.keyCode == 37? props.toggleMathType(false) : ''}} />
       <div id="mathType">
         <p>Add/Subtract</p>
         <p>Multiplication</p>
