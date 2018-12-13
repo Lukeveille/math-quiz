f79f1d4698f19d0f719957c9074c38cd5dc0f727
Lukeveille <luke@lukeleveille.com>
2018-12-13 10:17:32 -0500

    small debug

--- App.js
+++ App.js
@@ -5,9 +5,9 @@ function DefaultScreen(props) {
   return (
     <div>
       Highest integer you'd like to see:
-      <input ref={input => {input && props.focus === 'range'? (input.focus(), props.select? input.select() : '') : ''}} id="range" value={props.range} type="text" size="5" autoComplete="off" onChange={e => props.updateInput(e)} onKeyUp={e => {console.log(e); e.keyCode == 13? props.initialize() : e.keyCode == 40 || e.keyCode == 9? props.toggleFocus() : e.keyCode == 39? props.toggleMathType(true) : e.keyCode == 37? props.toggleMathType(false) : ''}} />
+      <input ref={input => {input && props.focus === 'range'? (input.focus(), props.select? input.select() : '') : ''}} id="range" value={props.range} type="text" size="5" autoComplete="off" onFocus={() => {props.focus != 'range'? props.toggleFocus() : ''}} onChange={e => props.updateInput(e)} onKeyUp={e => {console.log(e); e.keyCode == 13? props.initialize() : e.keyCode == 40 || e.keyCode == 9? props.toggleFocus() : e.keyCode == 39? props.toggleMathType(true) : e.keyCode == 37? props.toggleMathType(false) : ''}} />
       How many questions?
-      <input ref={input => {input && props.focus === 'questions'? (input.focus(), props.select? input.select() : '') : ''}} id="questions" value={props.questions} type="text" size="5" autoComplete="off" onChange={e => props.updateInput(e)} onKeyUp={e => {e.keyCode == 13? props.initialize() : e.keyCode == 38? props.toggleFocus() : e.keyCode == 39? props.toggleMathType(true) : e.keyCode == 37? props.toggleMathType(false) : ''}} />
+      <input ref={input => {input && props.focus === 'questions'? (input.focus(), props.select? input.select() : '') : ''}} id="questions" value={props.questions} type="text" size="5" autoComplete="off" onFocus={() => {props.focus != 'questions'? props.toggleFocus() : ''}} onChange={e => props.updateInput(e)} onKeyUp={e => {e.keyCode == 13? props.initialize() : e.keyCode == 38? props.toggleFocus() : e.keyCode == 39? props.toggleMathType(true) : e.keyCode == 37? props.toggleMathType(false) : ''}} />
       <div id="mathType">
         <p>Add/Subtract</p>
         <p>Multiplication</p>
